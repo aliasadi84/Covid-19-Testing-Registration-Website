34ca025843a5242efd61562a18d3774b14da1f2e
TimothySonofSam <71100745+TimothySonofSam@users.noreply.github.com>
2022-03-23 16:33:39 -0400

    comments for implementation steps

--- clinic/addresults.php
+++ clinic/addresults.php
@@ -1,1 +1,13 @@
+<?php
 
+# Pull appointments awaiting results (if status is not done) from DB and insert them in a dropdown menu
+# Allow user to select appointments
+# User should be able to select 'positive' or 'negative' as a test result
+# Confirmation pop-up should appear saying "Are you sure you want to enter <result> as the test result for <patient-name>?" to avoid error
+
+?>
+<!DOCTYPE html>
+
+<!-- CSS should be Madeline's newest -->
+
+</html>
