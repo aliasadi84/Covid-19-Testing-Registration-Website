c82978c047474d16719d934e9647270cedeea876
TimothySonofSam <71100745+TimothySonofSam@users.noreply.github.com>
2022-03-28 12:25:51 -0400

    Update appointment.php

--- patient/appointment.php
+++ patient/appointment.php
@@ -88,15 +88,17 @@ header("Location: patient.php");
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-	<!--css files-->
-	<script src="https://kit.fontawesome.com/95c473646d.js" crossorigin="anonymous"></script>
-	<link rel="stylesheet" href="assets/css/main.css">
-	<link rel="stylesheet" href="assets/css/button.css">
+    <script src="https://kit.fontawesome.com/95c473646d.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../assets/css/main.css">
+    <link rel="stylesheet" href="../assets/css/loginButton.css">
+	<link rel="stylesheet" href="../assets/css/submit.css">
+	<link rel="stylesheet" href="../assets/css/table.css">
+	<link rel="stylesheet" href="../assets/css/input.css">
 </head>
 
 <header>
     <div class="hero-image">
-    <a href="https://www.waynecountyhealthy.com" ><img src="assets/img/pp.png" width="100%"></a>
+        <a href="patient.php"><img src="../assets/pp.png" width="50%"></a>
     </div>
 </header>
 
@@ -108,92 +110,92 @@ header("Location: patient.php");
 <body>
   <section>
     <div class="bf">
-    <div class="center">
-	
-	
 	<form class="form" role="form" method="POST" accept-charset="UTF-8">
 		<div>Appointment Information</div>
 		<!--prints out the details of the appointment chosen-->
 		<div>
-			Date: <?php echo $date ?><br>
+			Date: <?php echo date('m/d/Y', strtotime($date)); ?><br>
 			Time: <?php echo $timeslot ?><br>
 		</div>
 		<!--covid-19 questionaire starts here-->
 
-		<div class="form-group">
+			
+		
 			<p>1. Regardless of your vaccination status, have
 				you experienced any of the symptoms in
-				the list below in the past 48 hours?
-			</p>
-
+				the list below in the past 48 hours?</p>
+			
+			
+			<div class="uniAlign">
 			<input type="checkbox" class= "largerCheckbox" id="symptom1" name="symp[]" value="fever or chills">
-			<label for="symptom1"> Fever or chills</label><br><br>
+			<label for="symptom1"> Fever or chills</label><br>
 			<input type="checkbox" class= "largerCheckbox" id="symptom2" name="symp[]" value="cough">
-			<label for="symptom2"> Cough</label><br><br>
+			<label for="symptom2"> Cough</label><br>
 			<input type="checkbox" class= "largerCheckbox" id="symptom3" name="symp[]" value="shortness of breath or difficulty breathing">
-			<label for="symptom3"> Shortness of breath or difficulty breathing</label><br><br>
+			<label for="symptom3"> Shortness of breath or difficulty breathing</label><br>
 			<input type="checkbox" class= "largerCheckbox" id="symptom4" name="symp[]" value="fatigue">
-			<label for="symptom4"> Fatigue</label><br><br>
+			<label for="symptom4"> Fatigue</label><br>
 			<input type="checkbox" class= "largerCheckbox" id="symptom5" name="symp[]" value="muscle or body aches">
-			<label for="symptom5"> Muscle or body aches</label><br><br>
+			<label for="symptom5"> Muscle or body aches</label><br>
 			<input type="checkbox" class= "largerCheckbox" id="symptom6" name="symp[]" value="headache">
-			<label for="symptom6"> Headache</label><br><br>
+			<label for="symptom6"> Headache</label><br>
 			<input type="checkbox" class= "largerCheckbox" id="symptom7" name="symp[]" value="new loss of taste or smell">
-			<label for="symptom7"> New loss of taste or smell</label><br><br>
+			<label for="symptom7"> New loss of taste or smell</label><br>
 			<input type="checkbox" class= "largerCheckbox" id="symptom8" name="symp[]" value="sore throat">
-			<label for="symptom8"> Sore throat</label><br><br>
+			<label for="symptom8"> Sore throat</label><br>
 			<input type="checkbox" class= "largerCheckbox" id="symptom9" name="symp[]" value="congestion or runny nose">
-			<label for="symptom9"> Congestion or runny nose</label><br><br>
+			<label for="symptom9"> Congestion or runny nose</label><br>
 			<input type="checkbox" class= "largerCheckbox" id="symptom10" name="symp[]" value="nausea or vomiting">
-			<label for="symptom10"> Nausea or vomiting</label><br><br>
+			<label for="symptom10"> Nausea or vomiting</label><br>
 			<input type="checkbox" class= "largerCheckbox" id="symptom11" name="symp[]" value="diarrhea">
 			<label for="symptom11"> Diarrhea</label><br>
-		</div>
+			</div>
 
-		<div class="form-group">
 			<p>2. Are you isolating or quarantining because you
 			tested positive for COVID-19 or are worried
 			that you may be sick with COVID-19?</p>
+			
 			  <input type="radio" class= "largerRadio" id="yes_isolating" name="isolating" value="yes">
-			  <label for="yes_isolating"> Yes</label><br><br>
+			  <label for="yes_isolating"> Yes</label><br>
 			  <input type="radio" class= "largerRadio" id="no_isolating" name="isolating" value="no">
 			  <label for="no_isolating"> No</label><br>
-		</div>
-
-		<div class="form-group">
-			<p>3. Have you been in close physical contact in the last 14 days with:</p>
+			
+		
+			<p>3. Have you been in close physical contact in the last 14 days with someone who has tested positive for COVID-19?</p>
+			 
 			  <input type="radio" class= "largerRadio" id="yes_contact" name="contact" value="yes">
-			  <label for="yes_contact"> Yes</label><br><br>
+			  <label for="yes_contact"> Yes</label><br>
 			  <input type="radio" class= "largerRadio" id="no_contact" name="contact" value="no">
 			  <label for="no_contact"> No</label><br>
-		</div>
+			
+		
 
-		<div class="form-group">
 			<p>4. Have you traveled in the past 10 days?</p>
+			
 			  <input type="radio" class= "largerRadio" id="yes_travel" name="travel" value="yes">
-			  <label for="yes_travel"> Yes</label><br><br>
+			  <label for="yes_travel"> Yes</label><br>
 			  <input type="radio" class= "largerRadio" id="no_travel" name="travel" value="no">
 			  <label for="no_travel"> No</label><br>
-		</div>
+			
 
-		<div class="form-group">
+
+
 		<p>5. Are you vaccinated for COVID-19?</p>
 		  <input type="radio" class= "largerRadio" id="yes_vaccinated" name="vaccinated" value="yes">
-		  <label for="yes_vaccinated"> Yes</label><br><br>
+		  <label for="yes_vaccinated"> Yes</label><br>
 		  <input type="radio" class= "largerRadio" id="no_vaccinated" name="vaccinated" value="no">
 		  <label for="no_vaccinated"> No</label><br><br>
-		</div>
+
 		<!--Covid-19 questionaire ends-->
         <!--when the submit button is clicked the input will be sent to the php at the top to be entered to the database-->
-		<div class="form-group">
-			<input type="submit" name="appointment" id="submit" class="btn btn-primary" value="Make Appointment">
-		</div>
-     
-	</form>
+	
+		<input type="submit" name="appointment" id="submit" value="Create Appointment">
 
+	</form>
 	</div>
-	</div>
+	
 </section>
+<br>
   
 </body>
 </html>
