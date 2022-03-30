2b96f56f37f9b03a41758708876acf3bd56ae75e
TimothySonofSam <71100745+TimothySonofSam@users.noreply.github.com>
2022-03-30 13:13:51 -0400

    Update nresultdb.php

--- clinic/nresultdb.php
+++ clinic/nresultdb.php
@@ -5,7 +5,7 @@ include_once '../assets/conn/dbconnect.php';
 $userid = $_GET['userid'];
 $chkYesNo = $_GET['chkYesNo'];
 
-$update = mysqli_query($con,"UPDATE bookings SET result='negetive' WHERE id=$userid");
+$update = mysqli_query($con,"UPDATE bookings SET result='Negative' WHERE id=$userid");
 $update = mysqli_query($con,"UPDATE bookings SET status='result entered' WHERE id=$userid");
 
 ?>
