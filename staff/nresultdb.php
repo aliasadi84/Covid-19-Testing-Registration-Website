364d91674cdf9731bbd63fb82c46629092e76ea1
SalemB-WSU <78177904+SalemB-WSU@users.noreply.github.com>
2022-03-30 13:36:01 -0400

    Add files via upload

--- staff/nresultdb.php
+++ staff/nresultdb.php
@@ -5,8 +5,9 @@ include_once '../assets/conn/dbconnect.php';
 $userid = $_GET['userid'];
 $chkYesNo = $_GET['chkYesNo'];
 
-$update = mysqli_query($con,"UPDATE bookings SET result='negetive' WHERE id=$userid AND result=''");
-$update = mysqli_query($con,"UPDATE bookings SET status='result entered' WHERE id=$userid AND result=''");
+
+$update = mysqli_query($con,"UPDATE bookings SET result='negetive' WHERE id=$userid");
+$update = mysqli_query($con,"UPDATE bookings SET status='result entered' WHERE id=$userid");
 
 $res=mysqli_query($con,"SELECT * FROM bookings WHERE id=$userid");
 $userRow=mysqli_fetch_array($res,MYSQLI_ASSOC);
