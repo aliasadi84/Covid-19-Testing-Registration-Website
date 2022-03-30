4601d0ed120610576c7f573fc9a79c512c2a55ed
madlyDarby <90344169+madlyDarby@users.noreply.github.com>
2022-03-29 22:28:27 -0400

    Update results.php

--- patient/results.php
+++ patient/results.php
@@ -24,7 +24,7 @@ $res=mysqli_query($con, "SELECT a.*, b.* FROM patient a
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
     <script src="https://kit.fontawesome.com/95c473646d.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../assets/css/main.css">
-    <link rel="stylesheet" href="../assets/css/loginButton.css">
+    <link rel="stylesheet" href="../assets/css/button.css">
 	<link rel="stylesheet" href="../assets/css/table.css">
 	<link rel="stylesheet" href="../assets/css/input.css">
 </head>
