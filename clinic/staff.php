662d4398a614ab1d221b2f7bd96c7931c4bb8a53
TimothySonofSam <71100745+TimothySonofSam@users.noreply.github.com>
2022-03-30 12:45:56 -0400

    Update staff.php

--- clinic/staff.php
+++ clinic/staff.php
@@ -22,10 +22,10 @@ $userRow=mysqli_fetch_array($res,MYSQLI_ASSOC);
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
     <script src="https://kit.fontawesome.com/95c473646d.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../assets/css/main.css">
-    <link rel="stylesheet" href="../assets/css/loginButton.css">
-	<link rel="stylesheet" href="../assets/css/table.css">
-	<link rel="stylesheet" href="../assets/css/input.css">
-  <link rel="stylesheet" href="../assets/css/submit.css">
+    <link rel="stylesheet" href="../assets/css/button.css">
+	  <link rel="stylesheet" href="../assets/css/table.css">
+	  <link rel="stylesheet" href="../assets/css/navbar.css">
+    <link rel="stylesheet" href="../assets/css/input.css">
 </head>
 
 <header>
@@ -34,7 +34,7 @@ $userRow=mysqli_fetch_array($res,MYSQLI_ASSOC);
     </div>
 </header>
 <body>
-<ul>
+    <ul>
       <li><a  href="doctordashboard.php">Dashboard</a></li>
       <li><a href="addresults.php">Add Result</a></li>
       <li><a href="patientlist.php">Patient List</a></li>
