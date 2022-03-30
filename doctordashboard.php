3aa233c88f04cf8114c4f8c0508e875776f9399d
SalemB-WSU <78177904+SalemB-WSU@users.noreply.github.com>
2022-03-30 06:48:10 -0400

    Add files via upload

---
+++ doctordashboard.php
@@ -0,0 +1,201 @@
+<?php
+session_start();
+include_once '../assets/conn/dbconnect.php';
+if(!isset($_SESSION['staffSession']))
+{
+//if not logged into the staff side it will direct you to the stafflogin.php
+header("Location: ../stafflogin.php");
+}
+$usersession = $_SESSION['staffSession'];
+//Checking the staff ID making sure it's still there
+$res=mysqli_query($con,"SELECT * FROM staff WHERE icstaff = '$usersession'");
+$userRow=mysqli_fetch_array($res,MYSQLI_ASSOC);
+
+
+//html needs to be redone
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <script src="https://kit.fontawesome.com/95c473646d.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../assets/css/main.css">
+    <link rel="stylesheet" href="../assets/css/loginButton.css">
+	<link rel="stylesheet" href="../assets/css/table.css">
+	<link rel="stylesheet" href="../assets/css/input.css">
+</head>
+
+<header>
+    <div class="hero-image">
+        <a href="patient.php"><img src="../assets/pp.png" width="50%"></a>
+    </div>
+</header>
+<body>
+<!-- Sidebar code needs to be changed -->
+<div class="sidebar">
+    <ul class="nav-list">
+      <li>
+         <a href="doctordashboard.php">
+          <i class='bx bx-grid-alt'></i>
+          <span class="links_name">Dashboard</span>
+        </a>
+      </li>
+     <li>
+       <a href="patientlist.php">
+       <i class='bx bx-user-pin'></i>
+         <span class="links_name">Patient List</span>
+       </a>
+     </li>
+     <li>
+         <!-- <a href="doctorprofile.php">
+         <i class='bx bx-user'></i>
+          <span class="links_name">Staff Profile</span>
+        </a> -->
+      </li>
+     <li class="profile">
+         <div class="profile-details">
+           <!--<img src="profile.jpg" alt="profileImg">-->
+           <div class="name_job">
+           </div>
+         </div>
+         <a href="logout.php?logout"><i class='bx bx-log-out' id="log_out" >Log Out</i></a>
+     </li>
+    </ul>
+  </div>
+  <section class="home-section">
+
+                <h2>
+                Dashboard
+                </h2>
+              
+    
+            <h3>Appointment List</h3>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Contact No.</th>
+                        <th>Email</th>
+                        <th>Date</th>
+                        <th>Time</th>
+                        <th>Status</th>
+                        <th>Complete</th>
+                    </tr>
+                </thead>
+              
+                
+                
+               <!-- Below code is populating the appointment table -->
+               <!-- First code block is pulling the data and comparing the data -->
+                <?php 
+                $res=mysqli_query($con,"SELECT a.*, b.*
+                                        FROM patient a
+                                        JOIN bookings b
+                                        On a.icPatient = b.username
+                                        Order By id desc");
+                      if (!$res) {
+                        printf("Error: %s\n", mysqli_error($con));
+                        exit();
+                    }
+                while ($appointment=mysqli_fetch_array($res)) {
+                    
+                
+                  //code for the check in box    
+                  if ($appointment['status']=='checked-in') {
+                        $status="danger";
+                        $icon='remove';
+                        $checked='';
+
+                    } else {
+                        $status="success";
+                        $icon='ok';
+                        $checked = 'disabled';
+                    }
+                    // Displaying the data, 
+                    echo "<tbody>";
+                    echo "<tr>";
+                        echo "<td>" . $appointment['patientFirstName'] . "</td>";
+                        echo "<td>" . $appointment['patientLastName'] . "</td>";
+                        echo "<td>" . $appointment['patientPhone'] . "</td>";
+                        echo "<td>" . $appointment['patientEmail'] . "</td>";
+                        echo "<td>" . $appointment['date'] . "</td>";
+                        echo "<td>" . $appointment['timeslot'] . "</td>";
+                        echo "<td>" . $appointment['status'] . "</td>";
+                        echo "<form method='POST'>";
+                        echo "<td ><input type='checkbox' name='enable' id='enable' value='".$appointment['id']."' onclick='chkit(".$appointment['id'].",this.checked);' ".$checked."></td>";
+
+                    
+                } 
+                    echo "</tr>";
+                echo "</tbody>";
+            echo "</table>";
+            echo "<div>";
+            echo "<div>";
+            echo "<button type='submit' value='Submit' name='submit'>Update</button>";
+            echo "</div>";
+            echo "</div>";
+            ?>
+  </section>
+                    <!-- panel end -->
+
+<!-- takes all the inputs from the checkbox, send to checkdatabase.php -->
+<script type="text/javascript">
+function chkit(uid, chk) {
+   chk = (chk==true ? "1" : "0");
+   var url = "checkdb.php?userid="+uid+"&chkYesNo="+chk;
+   if(window.XMLHttpRequest) {
+      req = new XMLHttpRequest();
+   } else if(window.ActiveXObject) {
+      req = new ActiveXObject("Microsoft.XMLHTTP");
+   }
+   // Use get instead of post.
+   req.open("GET", url, true);
+   req.send(null);
+}
+</script>
+                </div>
+                <!-- /.container-fluid -->
+            </div>
+            <!-- /#page-wrapper -->
+        
+        
+        
+        
+        
+        
+      
+          </div>
+
+
+          <!-- This is sidebar code to make it pretty, needs to removed to bettwr fit the theme -->
+          <script>
+          let sidebar = document.querySelector(".sidebar");
+          let closeBtn = document.querySelector("#btn");
+          let searchBtn = document.querySelector(".bx-search");
+
+          closeBtn.addEventListener("click", ()=>{
+            sidebar.classList.toggle("open");
+            menuBtnChange();//calling the function(optional)
+          });
+
+          searchBtn.addEventListener("click", ()=>{ // Sidebar open when you click on the search iocn
+            sidebar.classList.toggle("open");
+            menuBtnChange(); //calling the function(optional)
+          });
+
+          // following are the code to change sidebar button(optional)
+          function menuBtnChange() {
+          if(sidebar.classList.contains("open")){
+            closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");//replacing the iocns class
+          }else {
+            closeBtn.classList.replace("bx-menu-alt-right","bx-menu");//replacing the iocns class
+          }
+          }
+          </script>
+
+    </body>
+</html>
---
+++ patientlist.php
@@ -0,0 +1,148 @@
+<?php
+session_start();
+include_once '../assets/conn/dbconnect.php';
+if(!isset($_SESSION['staffSession']))
+{
+//if not logged in it will take you to the below location
+header("Location: ../stafflogin.php");
+}
+$usersession = $_SESSION['staffSession'];
+$res=mysqli_query($con,"SELECT * FROM staff WHERE icstaff = '$usersession'");
+$userRow=mysqli_fetch_array($res,MYSQLI_ASSOC);
+
+
+
+
+
+?>
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <script src="https://kit.fontawesome.com/95c473646d.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../assets/css/main.css">
+    <link rel="stylesheet" href="../assets/css/loginButton.css">
+	<link rel="stylesheet" href="../assets/css/table.css">
+	<link rel="stylesheet" href="../assets/css/input.css">
+</head>
+
+<header>
+    <div class="hero-image">
+        <a href="patient.php"><img src="../assets/pp.png" width="50%"></a>
+    </div>
+</header>
+<body>
+<!-- Sidebar code needs to be changed -->
+<div class="sidebar">
+    <ul class="nav-list">
+      <li>
+         <a href="doctordashboard.php">
+          <i class='bx bx-grid-alt'></i>
+          <span class="links_name">Dashboard</span>
+        </a>
+      </li>
+     <li>
+       <a href="patientlist.php">
+       <i class='bx bx-user-pin'></i>
+         <span class="links_name">Patient List</span>
+       </a>
+     </li>
+     <!-- <li>
+         <a href="doctorprofile.php">
+         <i class='bx bx-user'></i>
+          <span class="links_name">Staff Profile</span>
+        </a>
+      </li> -->
+     <li class="profile">
+         <div class="profile-details">
+           <!--<img src="profile.jpg" alt="profileImg">-->
+           <div class="name_job">
+           </div>
+         </div>
+         <a href="logout.php?logout"><i class='bx bx-log-out' id="log_out" >Log Out</i></a>
+     </li>
+    </ul>
+  </div>
+  <section class="home-section">
+
+
+                            <h2>
+                            Patient List
+                            </h2>
+ 
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Username</th>
+                                    <th>First Name</th>
+                                    <th>Last Name</th>
+                                    <th>Email</th>
+                                    <th>Phone Number</th>
+                                    <th>Gender</th>
+                                    <th>Birthdate</th>
+                                    <th>Race</th>
+                                </tr>
+                            </thead>
+                            
+                            <?php 
+                            $result=mysqli_query($con,"SELECT * FROM patient");
+                            
+
+                            //displaying the data from the datebase     
+                            while ($patientRow=mysqli_fetch_array($result)) {
+                                
+                              
+                                echo "<tbody>";
+                                echo "<tr>";
+                                    echo "<td>" . $patientRow['icPatient'] . "</td>";
+                                    echo "<td>" . $patientRow['patientFirstName'] . "</td>";
+                                    echo "<td>" . $patientRow['patientLastName'] . "</td>";
+                                    echo "<td>" . $patientRow['patientEmail'] . "</td>";
+                                    echo "<td>" . $patientRow['patientPhone'] . "</td>";
+                                    echo "<td>" . $patientRow['patientGender'] . "</td>";
+                                    echo "<td>" . $patientRow['patientDOB'] . "</td>";
+                                    echo "<td>" . $patientRow['race'] . "</td>";
+                                    echo "<form method='POST'>";
+                                    echo "<td class='text-center'><a href='#' id='".$patientRow['icPatient']."' class='delete'><span class='glyphicon glyphicon-trash' aria-hidden='true'></span></a>
+                            </td>";
+                               
+                            } 
+                                echo "</tr>";
+                           echo "</tbody>";
+                       echo "</table>";
+                       echo "<div>";
+                       echo "<div>";
+                       echo "<button class='btn btn-primary' type='submit' value='Submit' name='submit'>Update</button>";
+                        echo "</div>";
+                        echo "</div>";
+                        ?>
+
+                        </section>
+
+          <script>
+          let sidebar = document.querySelector(".sidebar");
+          let closeBtn = document.querySelector("#btn");
+          let searchBtn = document.querySelector(".bx-search");
+
+          closeBtn.addEventListener("click", ()=>{
+            sidebar.classList.toggle("open");
+            menuBtnChange();
+          });
+
+          searchBtn.addEventListener("click", ()=>{ 
+            sidebar.classList.toggle("open");
+            menuBtnChange(); 
+          });
+
+         
+          function menuBtnChange() {
+          if(sidebar.classList.contains("open")){
+            closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");
+          }else {
+            closeBtn.classList.replace("bx-menu-alt-right","bx-menu");
+          }
+          }
+          </script>
+
+    </body>
+</html>
