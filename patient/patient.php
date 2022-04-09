7122de38957ec9e0df2bc3b949aa777ed701e7a7
TimothySonofSam <71100745+TimothySonofSam@users.noreply.github.com>
2022-04-09 15:00:00 -0400

    Update patient.php

--- patient/patient.php
+++ patient/patient.php
@@ -61,7 +61,6 @@ $userRow=mysqli_fetch_array($res,MYSQLI_ASSOC);
 
 
 </body>
-
 <footer id="footer">
   <section class="hours">
     <br>
@@ -69,12 +68,9 @@ $userRow=mysqli_fetch_array($res,MYSQLI_ASSOC);
       <tr>
         <th><h3>Clinic Hours</h3></th>
       </tr>
+       
        <tr>
-        <td>Sunday</td>
-        <td>Closed</td>
-       </tr>
-       <tr>
-          <td>Monday-Friday</td>
+          <td>Monday-Tuesday</td>
           <td>9:00 am - 5:30 pm</td>
         </tr> 
         <tr>
@@ -82,9 +78,17 @@ $userRow=mysqli_fetch_array($res,MYSQLI_ASSOC);
           <td>11:00 am - 7:30 pm</td>
         </tr>
         <tr>
+          <td>Thursday-Friday</td>
+          <td>9:00 am - 5:30 pm</td>
+        </tr> 
+        <tr>
           <td>Saturday</td>
           <td>9:00 am - 1:00 pm</td>
         </tr>
+        <tr>
+          <td>Sunday</td>
+          <td>Closed</td>
+       </tr>
     </table>
     <br>
   </section>
@@ -95,19 +99,19 @@ $userRow=mysqli_fetch_array($res,MYSQLI_ASSOC);
             </tr>
             <tr>
                 <td>Phone</td>
-                <td>(313) 871-1926 x 0000</td><br>
+                <td><a href="tel:3138711926">(313) 871-1926</a></td><br>
               </tr>
             <tr>
               <td>Address</td>
-              <td>9021 Joseph Campau &bull; Hamtramck, MI 48212 &bull; USA</td>
+              <td><a href="https://goo.gl/maps/AYQRS5F6WrYikLVj7" target="_blank">9021 Joseph Campau &bull; Hamtramck, MI 48212 &bull; USA</a></td>
             </tr>
             </table>
             <br>
             <div>
-            <a href="https://www.paypal.com/fundraiser/charity/2121252" class="fa fa-paypal"></a>
-            <a href="https://www.facebook.com/WayneCountyHealthyCommunities/" class="fa fa-facebook"></a>
-            <a href="https://www.instagram.com/waynecountyhealthycommunities/" class="fa fa-instagram"></a>
-            <a href="https://www.linkedin.com/company/wayne-county-healhty-communities/" class="fa fa-linkedin"></a>
+            <a href="https://www.paypal.com/fundraiser/charity/2121252" target="_blank" class="fa fa-paypal"></a>
+            <a href="https://www.facebook.com/WayneCountyHealthyCommunities/" target="_blank" class="fa fa-facebook"></a>
+            <a href="https://www.instagram.com/waynecountyhealthycommunities/" target="_blank" class="fa fa-instagram"></a>
+            <a href="https://www.linkedin.com/company/wayne-county-healhty-communities/" target="_blank" class="fa fa-linkedin"></a>
           </div>
     </section>
 </footer>
