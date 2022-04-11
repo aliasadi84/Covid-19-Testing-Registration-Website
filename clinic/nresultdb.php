94570dc418d9ec2380fce8650d9bee1a275a1509
TimothySonofSam <71100745+TimothySonofSam@users.noreply.github.com>
2022-04-11 08:44:03 -0400

    changed subject line in email

--- clinic/nresultdb.php
+++ clinic/nresultdb.php
@@ -5,7 +5,7 @@ include_once '../assets/conn/dbconnect.php';
 $userid = $_GET['userid'];
 $chkYesNo = $_GET['chkYesNo'];
 
-$update = mysqli_query($con,"UPDATE bookings SET result='negetive' WHERE id=$userid");
+$update = mysqli_query($con,"UPDATE bookings SET result='Negative' WHERE id=$userid");
 $update = mysqli_query($con,"UPDATE bookings SET status='result entered' WHERE id=$userid");
 
 $res=mysqli_query($con,"SELECT * FROM bookings WHERE id=$userid");
@@ -18,11 +18,10 @@ $dateofappointment = $userRow['date'];
 $dateofappointment = date('m/d/Y',strtotime($dateofappointment));
 $timeslot = $userRow['timeslot'];
 
-
-
 $to = $userDow['patientEmail'];
-$subject = "COVID Test Results Available";
-$body ="Hello " .$userDow['patientFirstName']. ",\n\nThank you for testing with Wayne County Healthy Communities! The results of your COVID test on " . date('m/d/Y', strtotime($dateofappointment)) . " are now available.\n\nPlease log in to WCHC COVID-19 Portal to view results.";
+$subject = "Test Results Available";
+$body ="Hello " .$userDow['patientFirstName']. ",\n\nThank you for testing with Wayne County Healthy Communities! The results of your COVID test on " . date('F d, Y', strtotime($dateofappointment)) . " are now available.\n\nPlease log in to WCHC COVID-19 Portal to view results.";
 $header = "From: from@email";
 mail($to, $subject, $body, $header)
+
 ?>
