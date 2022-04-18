a82a21051d5c3d43437beb94caf1a733e56b6268
TimothySonofSam <71100745+TimothySonofSam@users.noreply.github.com>
2022-04-18 08:43:28 -0400

    Added the GCP connection in commented code

--- assets/conn/dbconnect.php
+++ assets/conn/dbconnect.php
@@ -1,8 +1,22 @@
 <?php
+/*$user=getenv('CLOUDSQL_USER');
+$pass=getenv('CLOUDSQL_PASSWORD');
+$inst=getenv('CLOUDSQL_DSN');
+$db=getenv('CLOUDSQL_DB');
+$con = mysqli_connect(null,$user,$pass,$db,null,$inst);
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }*/
+
+/* NOTE FROM LAST TEAM: When connecting to Google Cloud, comment out lines 15-20 */
+/* and uncomment 2-11 */
+
 $con = mysqli_connect("localhost","root","","sourcecodester_dadb");
 // Check connection
 if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
-  ?>
+?>
